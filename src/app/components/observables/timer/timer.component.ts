5f769d15a8b741f991733c923df198aa6c6814cc
milinisharma <74365456+milinisharma@users.noreply.github.com>
2025-07-04 07:07:38 +0530

    Added the observable type time

--- src/app/components/observables/timer/timer.component.ts
+++ src/app/components/observables/timer/timer.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { AddChocolatesService } from 'src/app/services/add-chocolates.service';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit, AfterViewInit{
+
+  orderMsg !: string;
+  orderSubscription !: Subscription;
+
+  constructor(
+    private pieService: AddChocolatesService
+  ) {}
+
+  ngOnInit() {}
+
+  addPie() {
+    console.log("Waiting for customer to start ordering!");
+    const pie = timer(3000, 1000);
+    this.orderSubscription = pie.subscribe((response: any) => {
+      console.log(response);
+      this.orderMsg = "Pie Ordered " + response;
+      this.pieService.addChocolate(this.orderMsg, 'pieList1');
+      this.pieService.addChocolate(this.orderMsg, 'pieList2');
+
+      if (response >= 10) {
+        this.orderSubscription.unsubscribe();
+      }
+    });
+  }
+
+  ngAfterViewInit() {}
+
 
 }
