4b489664a7f33c1a1cf5086e79eabc555210f300
milinisharma <74365456+milinisharma@users.noreply.github.com>
2025-07-13 17:32:20 +0530

    Added the of Observable

--- src/app/components/observables/of/of.component.ts
+++ src/app/components/observables/of/of.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { AddChocolatesService } from 'src/app/services/add-chocolates.service';
 
 @Component({
   selector: 'app-of',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./of.component.scss']
 })
 export class OfComponent {
+
+  cookieBaked: any;
+  cookieObj = {
+    a: "Cookie Baked 1",
+    b: "Cookie Baked 2",
+    c: "Cookie Baked 3",
+    d: "Cookie Baked 4",
+    e: "Cookie Baked 5"
+  }
+
+  cookiesReceived: any;
+
+  constructor(
+    private addChocolate: AddChocolatesService
+  ) {}
+
+  ngOnInit() {}
+
+  bakeCookies() {
+
+    const bakeCookie = of('Cookie Baked 1', 'Cookie Baked 2', 'Cookie Baked 3');
+    bakeCookie.subscribe((response: any) => {
+      this.addChocolate.addChocolate(response, 'cookieList1');
+    });
+
+    const bakeCookieObj = of(this.cookieObj);
+    bakeCookieObj.subscribe((response: any) => {
+      // this.addChocolate.addChocolate(response, 'cookieList2');
+      this.cookiesReceived = response;
+    })
+    
+  }
 
 }
