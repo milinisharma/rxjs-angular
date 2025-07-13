5d66c072bf60c1054d2b5e7029c608fcdc0102cc
milinisharma <74365456+milinisharma@users.noreply.github.com>
2025-07-13 19:56:48 +0530

    Added the from event for observable

--- src/app/components/observables/from/from.component.ts
+++ src/app/components/observables/from/from.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { AddChocolatesService } from 'src/app/services/add-chocolates.service';
 
 @Component({
   selector: 'app-from',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./from.component.scss']
 })
 export class FromComponent {
+  
+  constructor(
+    private chocolateService: AddChocolatesService
+  ) {}
+
+  ngOnInit() {}
+
+  bakeCroissants() {
+
+    let croissantsArr = [
+      "Croissant 1",
+      "Croissant 2",
+      "Croissant 3",
+      "Croissant 4",
+      "Croissant 5",
+    ]
+
+    const bakeCrossaint = from(croissantsArr);
+    bakeCrossaint.subscribe((response: any) => {
+      console.log(response);
+      this.chocolateService.addChocolate(response, 'croissantList1')
+    });
+
+    const bakeCroissantPromise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Croissants are baked through Promise")
+      }, 2000)
+    });
+
+    const bakeCroissantP = from(bakeCroissantPromise);
+    bakeCroissantP.subscribe((response: any) => {
+      console.log("Promise: ", response);
+      this.chocolateService.addChocolate(response, 'croissantList2');
+    });
+
+    const bakeCroissantString = from("Croissants are baked ~String");
+    bakeCroissantString.subscribe((response: any) => {
+      this.chocolateService.addChocolate(response, 'croissantList3')
+    })
+
+
+
+
+
+  }
 
 }
+function resolve(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
